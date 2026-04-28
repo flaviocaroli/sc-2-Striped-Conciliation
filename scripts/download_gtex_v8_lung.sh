bbc5fc4c9b5a06d8829b07763a201f6e966999b4
flaviocaroli <flaviocaroli3n9@gmail.com>
2026-04-28 13:56:45 +0200

    gtex download fix

--- scripts/download_gtex_v8_lung.sh
+++ scripts/download_gtex_v8_lung.sh
@@ -5,15 +5,17 @@ DATA_ROOT="${SC2_DATA_ROOT:-/home/3159436/sc2/data}"
 GTEX_RAW_DIR="${DATA_ROOT}/raw/gtex"
 mkdir -p "${GTEX_RAW_DIR}"
 
-# Standard public GTEx V8 files commonly used in public workflows.
-# If a URL stops working, replace it with the current link from:
-# https://www.gtexportal.org/home/downloads/adult-gtex/bulk_tissue_expression
+# These can be overridden at submit time if GTEx changes the paths again.
 SAMPLE_ATTR_URL="${SAMPLE_ATTR_URL:-https://storage.googleapis.com/gtex_analysis_v8/annotations/GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt}"
-EXPR_URL="${EXPR_URL:-https://storage.googleapis.com/gtex_analysis_v8/rna_seq_data/GTEx_Analysis_2017-06-05_v8_RSEMv1.3.0_gene_tpm.gct.gz}"
+EXPR_URL="${EXPR_URL:-https://storage.googleapis.com/gtex_analysis_v8/rna_seq_data/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_tpm.gct.gz}"
 
 SAMPLE_ATTR_FILE="${GTEX_RAW_DIR}/GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt"
-EXPR_FILE="${GTEX_RAW_DIR}/GTEx_Analysis_2017-06-05_v8_RSEMv1.3.0_gene_tpm.gct.gz"
+EXPR_FILE="${GTEX_RAW_DIR}/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_tpm.gct.gz"
 OUTPUT_H5AD="${GTEX_RAW_DIR}/gtex_lung.h5ad"
+
+echo "Checking GTEx URLs..."
+curl -I -L --fail "${SAMPLE_ATTR_URL}" | head
+curl -I -L --fail "${EXPR_URL}" | head
 
 echo "Downloading GTEx sample attributes..."
 curl -L --fail --retry 3 --retry-delay 5 -o "${SAMPLE_ATTR_FILE}" "${SAMPLE_ATTR_URL}"
